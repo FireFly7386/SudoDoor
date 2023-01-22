bba5ab71bed71f31f2a0d1fdb5023ad16b924f50
FireFly7386 <123244840+FireFly7386@users.noreply.github.com>
2023-01-22 17:12:42 +0100

    try something

--- SudoDoor.py
+++ SudoDoor.py
@@ -7,3 +7,5 @@ running = True
 
 while running:
     cmd = input(Colors.RESET + "(" + Colors.RED + "SudoDoor" + Colors.RESET + ") ")
+    if cmd == "listen":
+        listener.listen("0.0.0.0", 9001)
