42fe735996e0c78854e2fe7a324d665d68d952e1
FireFly7386 <123244840+FireFly7386@users.noreply.github.com>
2023-01-23 16:43:29 +0100

    Update portscan.py

--- portscan.py
+++ portscan.py
@@ -1,12 +1,10 @@
 import socket
 from colors import Colors
-from threading import Thread, Event
+from threading import Thread
 import math
 
-def scan_ports(ip, start_port, end_port, verbose, stop_event):
+def scan_ports(ip, start_port, end_port, verbose):
     for port in range(start_port, end_port):
-        if stop_event.is_set():
-            break
         try:
             sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             sock.settimeout(5)
@@ -24,29 +22,24 @@ def scan_ports(ip, start_port, end_port, verbose, stop_event):
             pass
 
 def scan_host(ip, start_port, end_port, num_threads, verbose):
-    total_ports = end_port - start_port
-    chunk_size = math.ceil(total_ports / num_threads)
 
-    threads = []
-    stop_event = Event()
-    for i in range(0, total_ports, chunk_size):
-        start = start_port + i
-        end = min(start_port + i + chunk_size, end_port)
-        t = Thread(target=scan_ports, args=(ip, start, end, verbose, stop_event))
-        t.start()
-        threads.append(t)
 
-    while True:
-        user_input = input('Press "q" to stop scanning: ')
-        if user_input == 'q':
-            stop_event.set()
-            break
+        total_ports = end_port - start_port
+        chunk_size = math.ceil(total_ports / num_threads)
 
-    for t in threads:
-        t.join()
+        threads = []
+        for i in range(0, total_ports, chunk_size):
+            start = start_port + i
+            end = min(start_port + i + chunk_size, end_port)
+            t = Thread(target=scan_ports, args=(ip, start, end, verbose))
+            t.start()
+            threads.append(t)
+       
+
+
 
 #ip = input("Enter the IP address or hostname to scan: ")
 #start_port = int(input("Enter the starting port to scan: "))
 #end_port = int(input("Enter the ending port to scan: "))
 #num_threads = int(input("Enter the number of threads to use: "))
-#scan_host(ip, start_port, end_port, num_threads, verbose=False)
+#scan_host(ip, start_port, end_port, num_threads)
